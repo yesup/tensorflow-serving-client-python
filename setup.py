e100db94538bbab8d4f5d776b4f8ad984e8a716d
Jeff Ye <jeffyecn@gmail.com>
2017-03-28 14:10:34 -0400

    fix release script

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
 setup(name='tensorflowservingclient',
-      version='0.5.1-1',
+      version='0.5.1-2',
       description='Prebuilt tensorflow serving client',
       url='https://github.com/yesup/tensorflow-serving-client-python',
       author='Jeff Ye',
       author_email='jeffye@yesup.com',
       license='Apache 2.0',
-      packages=['tensorflow_serving'],
+      packages=['tensorflow_serving.apis'],
+      install_requires=[
+            'grpcio',
+            'tensorflow'
+      ],
       zip_safe=False)
