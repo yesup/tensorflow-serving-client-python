c757b6808b9d315e129b9b4d1bd94c5a53a2698b
Jeff Ye <jeffyecn@gmail.com>
2017-03-28 13:32:57 -0400

    add package set up

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tensorflowservingclient',
+      version='0.5.1',
+      description='Prebuilt tensorflow serving client',
+      url='https://github.com/yesup/tensorflow-serving-client-python',
+      author='Jeff Ye',
+      author_email='jeffye@yesup.com',
+      license='Apache 2.0',
+      packages=['tensorflow_serving'],
+      zip_safe=False)
